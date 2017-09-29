9336abb8d9ad12ceccf548eef5cb63afa6aa9089
Marcos Ariel Rodrigues de Jesus <marcosariel.rj@hotmail.com>
2017-09-29 20:23:32 -0300

    "Mudança da letra do imc para minuscula"

--- src/main/java/mvc/ImcController.java
+++ src/main/java/mvc/ImcController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value = "/Imcmvc")
+@WebServlet(value = "/imcmvc")
 public class ImcController extends HttpServlet {
 	@Override
 	protected void doGet(
